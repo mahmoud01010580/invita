9f4d1a7894b4c594d9390b67530d047ca7fee5bd
Mahmoud Kroush <mahmoudkroush010@gmail.com>
2026-09-02 17:47:25 +0300

    Update app.js
    remove listen in server

--- Server/app.js
+++ Server/app.js
@@ -108,5 +108,4 @@ app.patch('/api/invitations/:slug/wishes/:wishId', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
